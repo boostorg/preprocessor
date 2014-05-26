70e0f2d6e0e3577c5783cda08f84f6f8319ca070
Edward Diener <eldiener@tropicsoft.com>
2014-05-26 16:36:30 -0400

    Using BOOST_PP_IDENTITY makes code clearer.

--- include/boost/preprocessor/facilities/is_empty.hpp
+++ include/boost/preprocessor/facilities/is_empty.hpp
@@ -24,7 +24,7 @@
 # include <boost/preprocessor/config/config.hpp>
 # if ~BOOST_PP_CONFIG_FLAGS() & BOOST_PP_CONFIG_MSVC() && ~BOOST_PP_CONFIG_FLAGS() & BOOST_PP_CONFIG_MWCC()
 # include <boost/preprocessor/tuple/elem.hpp>
-# include <boost/preprocessor/facilities/empty.hpp>
+# include <boost/preprocessor/facilities/identity.hpp>
 # else
 # include <boost/preprocessor/cat.hpp>
 # include <boost/preprocessor/detail/split.hpp>
@@ -35,7 +35,7 @@
 # if ~BOOST_PP_CONFIG_FLAGS() & BOOST_PP_CONFIG_MSVC() && ~BOOST_PP_CONFIG_FLAGS() & BOOST_PP_CONFIG_MWCC()
 #    define BOOST_PP_IS_EMPTY(x) BOOST_PP_IS_EMPTY_I(x BOOST_PP_IS_EMPTY_HELPER)
 #    define BOOST_PP_IS_EMPTY_I(contents) BOOST_PP_TUPLE_ELEM(2, 1, (BOOST_PP_IS_EMPTY_DEF_ ## contents()))
-#    define BOOST_PP_IS_EMPTY_DEF_BOOST_PP_IS_EMPTY_HELPER 1, 1 BOOST_PP_EMPTY
+#    define BOOST_PP_IS_EMPTY_DEF_BOOST_PP_IS_EMPTY_HELPER 1, BOOST_PP_IDENTITY(1)
 #    define BOOST_PP_IS_EMPTY_HELPER() , 0
 # else
 #    if BOOST_PP_CONFIG_FLAGS() & BOOST_PP_CONFIG_MSVC()

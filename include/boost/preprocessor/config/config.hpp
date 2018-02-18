00b2f4462445d7dd17bd11a8c8fdc26b2e41d653
Edward Diener <eldlistmailingz@tropicsoft.com>
2018-02-18 09:47:44 -0500

    Allow the IBM compiler to default to the level of C++/C compliance to determine default variadic macro support

--- include/boost/preprocessor/config/config.hpp
+++ include/boost/preprocessor/config/config.hpp
@@ -71,7 +71,7 @@
 # define BOOST_PP_VARIADICS_MSVC 0
 # if !defined BOOST_PP_VARIADICS
 #    /* variadic support explicitly disabled for all untested compilers */
-#    if defined __GCCXML__ || defined __PATHSCALE__ || defined __DMC__ || defined __CODEGEARC__ || defined __BORLANDC__ || defined __MWERKS__ || ( defined __SUNPRO_CC && __SUNPRO_CC < 0x5120 ) || defined __HP_aCC && !defined __EDG__ || defined __MRC__ || defined __SC__ || defined __IBMCPP__ || defined __PGI
+#    if defined __GCCXML__ || defined __PATHSCALE__ || defined __DMC__ || defined __CODEGEARC__ || defined __BORLANDC__ || defined __MWERKS__ || ( defined __SUNPRO_CC && __SUNPRO_CC < 0x5120 ) || defined __HP_aCC && !defined __EDG__ || defined __MRC__ || defined __SC__ || defined __PGI
 #        define BOOST_PP_VARIADICS 0
 #    elif defined(__CUDACC__)
 #        define BOOST_PP_VARIADICS 1

5756fb08adcd0cdfc2b8c130086e19f7572b32b9
Edward Diener <eldlistmailingz@tropicsoft.com>
2019-08-27 01:29:21 -0400

    Fixed inspect report problems: tabs and lack of copyright notices, and a few other small html file errors.

--- test/config_info.cpp
+++ test/config_info.cpp
@@ -1,3 +1,14 @@
+# /* **************************************************************************
+#  *                                                                          *
+#  *     (C) Copyright Edward Diener 2014.
+#  *     Distributed under the Boost Software License, Version 1.0. (See
+#  *     accompanying file LICENSE_1_0.txt or copy at
+#  *     http://www.boost.org/LICENSE_1_0.txt)
+#  *                                                                          *
+#  ************************************************************************** */
+#
+# /* See http://www.boost.org for most recent version. */
+#
 #include <iostream>
 #include <iomanip>
 #include <string.h>

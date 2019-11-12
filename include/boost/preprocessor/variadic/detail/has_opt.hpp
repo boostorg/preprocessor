a6404a8fb25d007baec912df3917b177e388fe3c
Edward Diener <eldlistmailingz@tropicsoft.com>
2019-11-12 14:57:09 -0500

    Fixed __VA_OPT__ functionality for vc++ in c20 mode.

--- include/boost/preprocessor/variadic/detail/has_opt.hpp
+++ include/boost/preprocessor/variadic/detail/has_opt.hpp
@@ -16,12 +16,21 @@
 #
 # if BOOST_PP_VARIADICS && defined(__cplusplus) && __cplusplus > 201703L
 #
+# if BOOST_PP_VARIADICS_MSVC
+# include <boost/preprocessor/cat.hpp>
+# endif
+#
 # define BOOST_PP_VARIADIC_HAS_OPT_FUNCTION(...) \
     __VA_OPT__(,) , 1, 0 \
 /**/
 #
+# if BOOST_PP_VARIADICS_MSVC
+# define BOOST_PP_VARIADIC_HAS_OPT_ELEM0(e0, ...) BOOST_PP_CAT(BOOST_PP_VARIADIC_HAS_OPT_ELEM_0(e0,__VA_ARGS__),)
+# define BOOST_PP_VARIADIC_HAS_OPT_ELEM2(e0, ...) BOOST_PP_CAT(BOOST_PP_VARIADIC_HAS_OPT_ELEM_2(e0,__VA_ARGS__),)
+# else
 # define BOOST_PP_VARIADIC_HAS_OPT_ELEM0(e0, ...) BOOST_PP_VARIADIC_HAS_OPT_ELEM_0(e0,__VA_ARGS__)
 # define BOOST_PP_VARIADIC_HAS_OPT_ELEM2(e0, ...) BOOST_PP_VARIADIC_HAS_OPT_ELEM_2(e0,__VA_ARGS__)
+# endif
 # define BOOST_PP_VARIADIC_HAS_OPT_ELEM_0(e0, ...) e0
 # define BOOST_PP_VARIADIC_HAS_OPT_ELEM_2(e0, e1, e2, ...) e2
 #

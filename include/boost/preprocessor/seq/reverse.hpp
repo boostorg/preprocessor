47b430e3d59af1962dc84bcb6a2a2728bffd25f7
Edward Diener <eldlistmailingz@tropicsoft.com>
2020-06-28 12:52:41 -0400

    Updated limits need undefs. Break reverse into two eequal parts. Updated seq tests.

--- include/boost/preprocessor/seq/reverse.hpp
+++ include/boost/preprocessor/seq/reverse.hpp
@@ -13,16 +13,28 @@
 # define BOOST_PREPROCESSOR_SEQ_REVERSE_HPP
 #
 # include <boost/preprocessor/config/config.hpp>
+# include <boost/preprocessor/cat.hpp>
+# include <boost/preprocessor/arithmetic/div.hpp>
+# include <boost/preprocessor/control/if.hpp>
 # include <boost/preprocessor/facilities/empty.hpp>
+# include <boost/preprocessor/facilities/identity.hpp>
+# include <boost/preprocessor/seq/first_n.hpp>
+# include <boost/preprocessor/seq/rest_n.hpp>
 # include <boost/preprocessor/seq/fold_left.hpp>
+# include <boost/preprocessor/seq/size.hpp>
 #
 # /* BOOST_PP_SEQ_REVERSE */
 #
 # if ~BOOST_PP_CONFIG_FLAGS() & BOOST_PP_CONFIG_EDG()
-#    define BOOST_PP_SEQ_REVERSE(seq) BOOST_PP_SEQ_FOLD_LEFT(BOOST_PP_SEQ_REVERSE_O, BOOST_PP_EMPTY, seq)()
+#    define BOOST_PP_SEQ_REVERSE(seq) BOOST_PP_SEQ_REVERSE_HALVES(BOOST_PP_DIV(BOOST_PP_SEQ_SIZE(seq),2),seq)
+#    define BOOST_PP_SEQ_REVERSE_HALVES(half,seq) \
+            BOOST_PP_IF(half,BOOST_PP_SEQ_REVERSE_NAMT,BOOST_PP_IDENTITY_N(seq,2))(half,seq)
+#    define BOOST_PP_SEQ_REVERSE_NAMT(half,seq) \
+            BOOST_PP_SEQ_DO_REVERSE(BOOST_PP_SEQ_REST_N(half,seq)) BOOST_PP_SEQ_DO_REVERSE(BOOST_PP_SEQ_FIRST_N(half,seq))
+#    define BOOST_PP_SEQ_DO_REVERSE(seq) BOOST_PP_SEQ_FOLD_LEFT(BOOST_PP_SEQ_REVERSE_O, BOOST_PP_EMPTY, seq)()
 # else
 #    define BOOST_PP_SEQ_REVERSE(seq) BOOST_PP_SEQ_REVERSE_I(seq)
-#    define BOOST_PP_SEQ_REVERSE_I(seq) BOOST_PP_SEQ_FOLD_LEFT(BOOST_PP_SEQ_REVERSE_O, BOOST_PP_EMPTY, seq)()
+#    define BOOST_PP_SEQ_REVERSE_I(seq) BOOST_PP_SEQ_REVERSE_HALVES(BOOST_PP_DIV(BOOST_PP_SEQ_SIZE(seq),2),seq)
 # endif
 #
 # define BOOST_PP_SEQ_REVERSE_O(s, state, elem) (elem) state
@@ -30,10 +42,15 @@
 # /* BOOST_PP_SEQ_REVERSE_S */
 #
 # if ~BOOST_PP_CONFIG_FLAGS() & BOOST_PP_CONFIG_EDG()
-#    define BOOST_PP_SEQ_REVERSE_S(s, seq) BOOST_PP_SEQ_FOLD_LEFT_ ## s(BOOST_PP_SEQ_REVERSE_O, BOOST_PP_EMPTY, seq)()
+#    define BOOST_PP_SEQ_REVERSE_S(s, seq) BOOST_PP_SEQ_REVERSE_HALVES_S(s,BOOST_PP_DIV(BOOST_PP_SEQ_SIZE(seq),2),seq)
+#    define BOOST_PP_SEQ_REVERSE_HALVES_S(s,half,seq) \
+            BOOST_PP_IF(half,BOOST_PP_SEQ_REVERSE_NAMT_S,BOOST_PP_IDENTITY_N(seq,3))(s,half,seq)
+#    define BOOST_PP_SEQ_REVERSE_NAMT_S(s,half,seq) \
+            BOOST_PP_SEQ_DO_REVERSE_S(s,BOOST_PP_SEQ_REST_N(half,seq)) BOOST_PP_SEQ_DO_REVERSE_S(s,BOOST_PP_SEQ_FIRST_N(half,seq))
+#    define BOOST_PP_SEQ_DO_REVERSE_S(s, seq) BOOST_PP_SEQ_FOLD_LEFT_ ## s(BOOST_PP_SEQ_REVERSE_O, BOOST_PP_EMPTY, seq)()
 # else
 #    define BOOST_PP_SEQ_REVERSE_S(s, seq) BOOST_PP_SEQ_REVERSE_S_I(s, seq)
-#    define BOOST_PP_SEQ_REVERSE_S_I(s, seq) BOOST_PP_SEQ_FOLD_LEFT_ ## s(BOOST_PP_SEQ_REVERSE_O, BOOST_PP_EMPTY, seq)()
+#    define BOOST_PP_SEQ_REVERSE_S_I(s, seq) BOOST_PP_SEQ_REVERSE_HALVES_S(s,BOOST_PP_DIV(BOOST_PP_SEQ_SIZE(seq),2),seq)
 # endif
 #
 # endif

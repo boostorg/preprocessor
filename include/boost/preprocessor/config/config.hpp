7bed5072e4f46ced42d1affe18906f84481efe2a
Edward Diener <eldlistmailingz@tropicsoft.com>
2020-09-22 00:30:33 -0400

    Update on config.hpp so we are not redefining a defined macro.

--- include/boost/preprocessor/config/config.hpp
+++ include/boost/preprocessor/config/config.hpp
@@ -83,9 +83,10 @@
 #    undef BOOST_PP_VARIADICS_MSVC
 # endif
 # define BOOST_PP_VARIADICS 1
-# define BOOST_PP_VARIADICS_MSVC 0
 # if defined _MSC_VER && _MSC_VER >= 1400 && !defined(__clang__) && (defined(__INTELLISENSE__) || (defined(__INTEL_COMPILER) && __INTEL_COMPILER >= 1700) || !(defined __EDG__ || defined __GCCXML__ || (defined __NVCC__ && defined __CUDACC__) || defined __PATHSCALE__ || defined __DMC__ || defined __CODEGEARC__ || defined __BORLANDC__ || defined __MWERKS__ || defined __SUNPRO_CC || defined __HP_aCC || defined __MRC__ || defined __SC__ || defined __IBMCPP__ || defined __PGI)) && (!defined(_MSVC_TRADITIONAL) || _MSVC_TRADITIONAL)
 #     define BOOST_PP_VARIADICS_MSVC 1
+# else
+#     define BOOST_PP_VARIADICS_MSVC 0
 # endif
 #
 # if BOOST_PP_CONFIG_FLAGS() & BOOST_PP_CONFIG_STRICT()
